1d977bd92cfb90b40f6c5fadf3d2e9fbb71d7fd6
chenyi <chenyi@globalegrow.com>
2020-02-29 18:38:44 +0800

    审核功能完善

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"https://api.liongolden.com/bg/admin/api/"', // plm正式机地址
+    // BASE_API: '"https://api.liongolden.com/bg/admin/api/"', // ZT正式机地址
+    BASE_API: '"http://129.211.181.211/bg/admin/api"', // ZT测试机地址
 }
