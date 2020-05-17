7801838e74d9a16a63c4faab0bcc19fc0b0245ee
hellocy <627829480@qq.com>
2020-05-17 21:51:39 +0800

    功能完善

---
+++ src/router/layout/index.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        path: '/layout',
+        meta: {
+            title: '而已测试'
+        },
+        component: () => import('@/views/layout/index')
+    }
+]
---
+++ src/views/layout/index.vue
@@ -0,0 +1,66 @@
+<style lang="scss" scoped>
+.row {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    width: 100%;
+}
+.col {
+    margin: 0;
+    margin-top: 5px;
+    padding: 10px;
+    width: 50%;
+    border: 1px solid #ddd;
+    box-sizing: border-box;
+}
+.from-control {
+    width: 100%;
+}
+</style>
+
+<template>
+    <div>
+        <div class="row">
+            <div class="col">
+                <el-select class="from-control">
+                    <el-option label="1111">1111</el-option>
+                    <el-option label="1111">1111</el-option>
+                    <el-option label="1111">1111</el-option>
+                    <el-option label="1111">1111</el-option>
+                </el-select>
+            </div>
+            <div class="col">
+                <el-select class="from-control">
+                    <el-option label="1111">1111</el-option>
+                    <el-option label="1111">1111</el-option>
+                    <el-option label="1111">1111</el-option>
+                    <el-option label="1111">1111</el-option>
+                </el-select>
+            </div>
+            <div class="col">4</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+            arrowIcon,
+            subRouterList: []
+        }
+    },
+    computed: {},
+    created() {
+        this.$store.dispatch('common/leftMenuVisible', true)
+    },
+    mounted: {},
+    methods: {
+        linkToPage(item) {
+            let url = item.url
+            this.$store.dispatch('setOpenedTabsPush', item)
+            url && this.$router.push(url)
+        }
+    }
+}
+</script>
